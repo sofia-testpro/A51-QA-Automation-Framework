04402f533d6cd374bcbafc9a8be2a3043e3d13c6
lolitasmann <149527279+lolitamann@users.noreply.github.com>
2023-11-09 13:36:15 -0500

    Homework 23
    Fixed homework 18 and homework 22. my homework 23 fails i need help

---
+++ src/test/java/PageObjectModel/AllSongsPage.java
@@ -0,0 +1,24 @@
+package PageObjectModel;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class AllSongsPage extends BasePage{
+
+    public AllSongsPage(WebDriver givenDriver){
+        super(givenDriver);
+    }
+
+    public void contextClickFirstSong(){
+        WebElement firstSongElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".all-songs tr.song-item:nth-child(1)")));
+        actions.contextClick(firstSongElement).perform();
+    }
+    public void choosePlayOption(){
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("li.playback"))).click();
+    }
+    public boolean isSongPlaying(){
+        WebElement soundBarVisualizer = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[data-testid='sound-bar-play']")));
+        return soundBarVisualizer.isDisplayed();
+    }
+}
--- src/test/java/PageObjectModel/HomePage.java
+++ src/test/java/PageObjectModel/HomePage.java
@@ -1,19 +1,53 @@
 package PageObjectModel;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class HomePage extends BasePage{
+    String newNameForPlaylist = "test";
 
     public HomePage(WebDriver givenDriver) {
         super(givenDriver);
     }
-    //locators
-    By userAvatarIcon = By.cssSelector("img.avatar");
+
+    //LOCATORS SELENIUM PAGE FACTORY
+    @FindBy(css = "[.playlist:nth-child(3)]")
+    WebElement selectPlaylist;
+    @FindBy(css = "[name='name']")
+    WebElement newPlaylistNameField;
 
     //Helpers
+    public void doubleClickOnSelectedPlaylist() {
+        actions.doubleClick(selectPlaylist).perform();
+    }
+    public HomePage enterNewNameForPlaylist(){
+        newPlaylistNameField.sendKeys(Keys.chord(Keys.COMMAND,"A",Keys.BACK_SPACE));
+        newPlaylistNameField.sendKeys(newNameForPlaylist);
+        newPlaylistNameField.sendKeys(Keys.ENTER);
+        return this;
+    }
+
+
+
+    //locators
+    private By userAvatarIcon = By.cssSelector("img.avatar");
+
+
     public WebElement getUserAvatar(){
         return findElement(userAvatarIcon);
+    }
+
+    public void chooseAllSongsList(){
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("li a.songs"))).click();
+    }
+    public WebElement hoverPlay() {
+        WebElement play= driver.findElement(By.cssSelector("[data-testid='play-btn']"));
+        // WebElement play = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[data-testid='play-btn']")));
+        actions.moveToElement(play).perform();
+        return wait.until(ExpectedConditions.visibilityOf(play));
     }
 }
